41f3f8c00cf0bff14e894c815a76dcdc35462ed0
Michael <mikebgoldman95@gmail.com>
2026-07-27 11:09:59 +0200

    Configured two AI model chat clients (openAI and anthropic)

--- backend/src/main/java/dev/michaelgoldman/notesbackend/NotesbackendApplication.java
+++ backend/src/main/java/dev/michaelgoldman/notesbackend/NotesbackendApplication.java
@@ -1,13 +1,15 @@
 package dev.michaelgoldman.notesbackend;
 
+import dev.michaelgoldman.notesbackend.config.ChatClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ChatClientConfig.class)
 public class NotesbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesbackendApplication.class, args);
 	}
-
 }
---
+++ backend/src/main/java/dev/michaelgoldman/notesbackend/config/ChatClientConfig.java
@@ -0,0 +1,20 @@
+package dev.michaelgoldman.notesbackend.config;
+
+import org.springframework.ai.anthropic.AnthropicChatModel;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel chatModel) {
+        return ChatClient.create(chatModel);
+    }
+
+    @Bean
+    public ChatClient anthropicChatClient(AnthropicChatModel chatModel) {
+        return ChatClient.create(chatModel);
+    }
+}
