0638d97c61706d9dcdd1e4c8a79fd9029b2895ed
Michael <mikebgoldman95@gmail.com>
2026-08-10 15:41:09 +0200

    Update db-diagram image; created V1 flyway migration file (creation of entries table)

---
+++ backend/src/main/resources/db/migration/V1__entries_create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE entries (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    version BIGINT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(20000) NOT NULL,
+    summary VARCHAR(500),
+    mood VARCHAR(20) CONSTRAINT entries_mood_check CHECK(mood IN ('HAPPY', 'CALM', 'NEUTRAL', 'ANXIOUS', 'SAD', 'FRUSTRATED')),
+    created_at TIMESTAMPTZ NOT NULL,
+    last_updated TIMESTAMPTZ NOT NULL,
+    analysed_at TIMESTAMPTZ
+);
