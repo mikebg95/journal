eeea86a7159fe25fd3718a2d54a79db7e55dfc37
Michael <mikebgoldman95@gmail.com>
2026-08-12 14:57:24 +0200

    Created Mood enum via TDD: created MoodTest.java. Contains two parameterized tests: one that tests converting "raw" recognizable moods to valid moods; the other tests that unrecognizable or null/blank/empty moods return an empty optional.

---
+++ backend/src/main/java/dev/michaelgoldman/journalbackend/domain/model/Mood.java
@@ -0,0 +1,32 @@
+package dev.michaelgoldman.journalbackend.domain.model;
+
+import java.util.Locale;
+import java.util.Optional;
+
+public enum Mood {
+    HAPPY, CALM, NEUTRAL, ANXIOUS, SAD, FRUSTRATED;
+
+    public static Optional<Mood> from(String raw) {
+        if (raw == null) {
+            return Optional.empty();
+        }
+
+        String cleaned = clean(raw);
+
+        if (cleaned.isBlank()) {
+            return Optional.empty();
+        }
+
+        for (Mood mood : values()) {
+            if (mood.name().equals(cleaned)) {
+                return Optional.of(mood);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static String clean(String uncleaned) {
+        return uncleaned.strip().toUpperCase(Locale.ROOT);
+    }
+}
---
+++ backend/src/test/java/dev/michaelgoldman/journalbackend/domain/model/MoodTest.java
@@ -0,0 +1,70 @@
+package dev.michaelgoldman.journalbackend.domain.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class MoodTest {
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("recognizableValues")
+    void whenValueIsRecognizable_shouldReturnMatchingMood(String name, Mood expected, String raw) {
+        assertEquals(Optional.of(expected), Mood.from(raw));
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("blankOrUnrecognizableValues")
+    void whenValueIsBlankOrUnrecognizable_shouldReturnEmptyOptional(String name, String value) {
+        assertEquals(Optional.empty(), Mood.from(value));
+    }
+
+    static Stream<Arguments> recognizableValues() {
+        return Stream.of(
+                arguments(
+                        "Case normalisation",
+                        Mood.HAPPY,
+                        "happy"
+                ),
+                arguments(
+                        "Trim start and end",
+                        Mood.FRUSTRATED,
+                        "   FRUSTRATED  "
+                )
+        );
+    }
+
+    static Stream<Arguments> blankOrUnrecognizableValues() {
+        return Stream.of(
+                arguments(
+                        "Null text",
+                        null
+                ),
+                arguments(
+                        "Empty string",
+                        ""
+                ),
+                arguments(
+                        "Whitespaces",
+                        "    "
+                ),
+                arguments(
+                        "Tab",
+                        "\t"
+                ),
+                arguments(
+                        "Unrecognizable",
+                        "ECSTATIC"
+                ),
+                arguments(
+                        "Almost a mood",
+                        "VERY HAPPY"
+                )
+        );
+    }
+}
